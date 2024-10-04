68d01ccfa7d62033dbc56e0f61c78094c6c30104
anandanlk <reach2anandan@gmail.com>
2024-10-04 08:05:57 +0200

    log file update

--- tests/logging_fixture.py
+++ tests/logging_fixture.py
@@ -6,12 +6,15 @@ import logging
 @pytest.fixture(scope="session", autouse=True)
 def configure_logging():
     current_dir = os.path.dirname(__file__)
-    log_dir = os.path.join(current_dir, '..', '..', '..', 'logs')
+    log_dir = os.path.join(current_dir, '..', 'logs')
 
     # Create log directory if it doesn't exist
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
     log_path = os.path.join(log_dir, 'test_logs.log')
+
+    # for handler in logging.root.handlers[:]:
+    #     logging.root.removeHandler(handler)
 
     logging.basicConfig(filename=log_path, filemode='w',
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
